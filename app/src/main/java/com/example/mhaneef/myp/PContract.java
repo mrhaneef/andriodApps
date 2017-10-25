7bc953e20c2e39643787b32069a3fb76528b377d
M H <mrhaneef@gmail.com>
2017-10-25 15:00:10 -0500

    added history table columns

--- app/src/main/java/com/example/mhaneef/myp/PContract.java
+++ app/src/main/java/com/example/mhaneef/myp/PContract.java
@@ -21,4 +21,17 @@ public class PContract {
     }
 
 
+
+    public static class HistoryEntry implements BaseColumns {
+        public static final String TABLE_NAME = "P";
+        public static final String COLUMN_NAME_F = "F";
+        public static final String COLUMN_NAME_Z = "Z";
+        public static final String COLUMN_NAME_A = "A";
+        public static final String COLUMN_NAME_M = "M";
+        public static final String COLUMN_NAME_I = "I";
+        public static final String COLUMN_NAME_CHANNGED = "CHANGED_COLUMN_NAME";
+        public static final String COLUMN_NAME_MODIFIED = "MODIFIED";
+    }
+
+
 }
